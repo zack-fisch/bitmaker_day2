b9338011d1074a9df1792623a1393e2eefb568de
Zack Fisch <zack@zfisch.com>
2014-06-03 12:12:57 -0400

    second commit

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+# Question 1, how would you calculate a good tip for a 55 dollar meal?
+def tip(cost)
+	cost += (cost * 0.15)
+end
+
+puts tip(55)
+
+# Question 2, add a string and integer
+x = 2.to_s + "two"
+
+puts x
+
+#Question 3
+
+result = 45628 * 7839
+
+puts "The result of the multiplication is #{result}"
+
+

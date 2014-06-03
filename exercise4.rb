2bb5fe9ff48468e7b29bf872a4dad93a93ba8b81
Zack Fisch <zack@zfisch.com>
2014-06-03 12:36:49 -0400

    adding exercise4

---
+++ exercise4.rb
@@ -0,0 +1,14 @@
+counter = 0
+
+while counter < 101
+	if counter % 3 == 0
+		puts "Bit"
+		counter += 1
+	elsif counter % 5 == 0
+		puts "Maker"
+		counter += 1
+	else
+	puts counter
+	counter += 1
+	end
+end
